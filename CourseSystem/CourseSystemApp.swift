21195b329c3f7f955dfc2e4056f50f081da2825e
derrick921213 <dlin12457@gmail.com>
2024-05-06 07:01:00 +0800

    fix

--- CourseSystem/CourseSystemApp.swift
+++ CourseSystem/CourseSystemApp.swift
@@ -6,7 +6,34 @@
 //
 
 import SwiftUI
-
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = Double((rgbValue >> 16) & 0xFF) / 255.0
+        let g = Double((rgbValue >> 8) & 0xFF) / 255.0
+        let b = Double(rgbValue & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b)
+    }
+}
+extension UIColor {
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = CGFloat((rgbValue >> 16) & 0xFF) / 255.0
+        let g = CGFloat((rgbValue >> 8) & 0xFF) / 255.0
+        let b = CGFloat(rgbValue & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
+}
 @main
 struct CourseSystemApp: App {
     let persistenceController = PersistenceController.shared
